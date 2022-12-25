13923ce3a7d2f578dea7fbb2cff9a216f4e8f12d
Rajan Chaudhary <rajann44@gmail.com>
2022-12-25 20:56:00 +0530

    updated pojo from jsonschema2pojo and fixed failing test

--- src/main/java/pojo/Filter.java
+++ src/main/java/pojo/Filter.java
@@ -1,261 +1,261 @@
 package pojo;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 /*******JSON Mapping to Pojo Class for api/v3/exchangeInfo********/
 public class Filter {
 
+    @JsonProperty("filterType")
     private String filterType;
+    @JsonProperty("minPrice")
     private String minPrice;
+    @JsonProperty("maxPrice")
     private String maxPrice;
+    @JsonProperty("tickSize")
     private String tickSize;
-    private String multiplierUp;
-    private String multiplierDown;
-    private Long avgPriceMins;
+    @JsonProperty("minQty")
     private String minQty;
+    @JsonProperty("maxQty")
     private String maxQty;
+    @JsonProperty("stepSize")
     private String stepSize;
+    @JsonProperty("minNotional")
     private String minNotional;
+    @JsonProperty("applyToMarket")
     private Boolean applyToMarket;
-    private Long limit;
-    private Long maxNumOrders;
-    private Long maxNumAlgoOrders;
+    @JsonProperty("avgPriceMins")
+    private Integer avgPriceMins;
+    @JsonProperty("limit")
+    private Integer limit;
+    @JsonProperty("minTrailingAboveDelta")
+    private Integer minTrailingAboveDelta;
+    @JsonProperty("maxTrailingAboveDelta")
+    private Integer maxTrailingAboveDelta;
+    @JsonProperty("minTrailingBelowDelta")
+    private Integer minTrailingBelowDelta;
+    @JsonProperty("maxTrailingBelowDelta")
+    private Integer maxTrailingBelowDelta;
+    @JsonProperty("bidMultiplierUp")
+    private String bidMultiplierUp;
+    @JsonProperty("bidMultiplierDown")
+    private String bidMultiplierDown;
+    @JsonProperty("askMultiplierUp")
+    private String askMultiplierUp;
+    @JsonProperty("askMultiplierDown")
+    private String askMultiplierDown;
+    @JsonProperty("maxNumOrders")
+    private Integer maxNumOrders;
+    @JsonProperty("maxNumAlgoOrders")
+    private Integer maxNumAlgoOrders;
 
-    /**
-     * No args constructor for use in serialization
-     *
-     */
-    public Filter() {
-    }
-
-    /**
-     *
-     * @param maxNumOrders
-     * @param multiplierDown
-     * @param multiplierUp
-     * @param stepSize
-     * @param applyToMarket
-     * @param tickSize
-     * @param minNotional
-     * @param minPrice
-     * @param limit
-     * @param avgPriceMins
-     * @param maxPrice
-     * @param filterType
-     * @param maxQty
-     * @param maxNumAlgoOrders
-     * @param minQty
-     */
-    public Filter(String filterType, String minPrice, String maxPrice, String tickSize, String multiplierUp, String multiplierDown, Long avgPriceMins, String minQty, String maxQty, String stepSize, String minNotional, Boolean applyToMarket, Long limit, Long maxNumOrders, Long maxNumAlgoOrders) {
-        super();
-        this.filterType = filterType;
-        this.minPrice = minPrice;
-        this.maxPrice = maxPrice;
-        this.tickSize = tickSize;
-        this.multiplierUp = multiplierUp;
-        this.multiplierDown = multiplierDown;
-        this.avgPriceMins = avgPriceMins;
-        this.minQty = minQty;
-        this.maxQty = maxQty;
-        this.stepSize = stepSize;
-        this.minNotional = minNotional;
-        this.applyToMarket = applyToMarket;
-        this.limit = limit;
-        this.maxNumOrders = maxNumOrders;
-        this.maxNumAlgoOrders = maxNumAlgoOrders;
-    }
-
+    @JsonProperty("filterType")
     public String getFilterType() {
         return filterType;
     }
 
+    @JsonProperty("filterType")
     public void setFilterType(String filterType) {
         this.filterType = filterType;
     }
 
-    public Filter withFilterType(String filterType) {
-        this.filterType = filterType;
-        return this;
-    }
-
+    @JsonProperty("minPrice")
     public String getMinPrice() {
         return minPrice;
     }
 
+    @JsonProperty("minPrice")
     public void setMinPrice(String minPrice) {
         this.minPrice = minPrice;
     }
 
-    public Filter withMinPrice(String minPrice) {
-        this.minPrice = minPrice;
-        return this;
-    }
-
+    @JsonProperty("maxPrice")
     public String getMaxPrice() {
         return maxPrice;
     }
 
+    @JsonProperty("maxPrice")
     public void setMaxPrice(String maxPrice) {
         this.maxPrice = maxPrice;
     }
 
-    public Filter withMaxPrice(String maxPrice) {
-        this.maxPrice = maxPrice;
-        return this;
-    }
-
+    @JsonProperty("tickSize")
     public String getTickSize() {
         return tickSize;
     }
 
+    @JsonProperty("tickSize")
     public void setTickSize(String tickSize) {
         this.tickSize = tickSize;
     }
 
-    public Filter withTickSize(String tickSize) {
-        this.tickSize = tickSize;
-        return this;
-    }
-
-    public String getMultiplierUp() {
-        return multiplierUp;
-    }
-
-    public void setMultiplierUp(String multiplierUp) {
-        this.multiplierUp = multiplierUp;
-    }
-
-    public Filter withMultiplierUp(String multiplierUp) {
-        this.multiplierUp = multiplierUp;
-        return this;
-    }
-
-    public String getMultiplierDown() {
-        return multiplierDown;
-    }
-
-    public void setMultiplierDown(String multiplierDown) {
-        this.multiplierDown = multiplierDown;
-    }
-
-    public Filter withMultiplierDown(String multiplierDown) {
-        this.multiplierDown = multiplierDown;
-        return this;
-    }
-
-    public Long getAvgPriceMins() {
-        return avgPriceMins;
-    }
-
-    public void setAvgPriceMins(Long avgPriceMins) {
-        this.avgPriceMins = avgPriceMins;
-    }
-
-    public Filter withAvgPriceMins(Long avgPriceMins) {
-        this.avgPriceMins = avgPriceMins;
-        return this;
-    }
-
+    @JsonProperty("minQty")
     public String getMinQty() {
         return minQty;
     }
 
+    @JsonProperty("minQty")
     public void setMinQty(String minQty) {
         this.minQty = minQty;
     }
 
-    public Filter withMinQty(String minQty) {
-        this.minQty = minQty;
-        return this;
-    }
-
+    @JsonProperty("maxQty")
     public String getMaxQty() {
         return maxQty;
     }
 
+    @JsonProperty("maxQty")
     public void setMaxQty(String maxQty) {
         this.maxQty = maxQty;
     }
 
-    public Filter withMaxQty(String maxQty) {
-        this.maxQty = maxQty;
-        return this;
-    }
-
+    @JsonProperty("stepSize")
     public String getStepSize() {
         return stepSize;
     }
 
+    @JsonProperty("stepSize")
     public void setStepSize(String stepSize) {
         this.stepSize = stepSize;
     }
 
-    public Filter withStepSize(String stepSize) {
-        this.stepSize = stepSize;
-        return this;
-    }
-
+    @JsonProperty("minNotional")
     public String getMinNotional() {
         return minNotional;
     }
 
+    @JsonProperty("minNotional")
     public void setMinNotional(String minNotional) {
         this.minNotional = minNotional;
     }
 
-    public Filter withMinNotional(String minNotional) {
-        this.minNotional = minNotional;
-        return this;
-    }
-
+    @JsonProperty("applyToMarket")
     public Boolean getApplyToMarket() {
         return applyToMarket;
     }
 
+    @JsonProperty("applyToMarket")
     public void setApplyToMarket(Boolean applyToMarket) {
         this.applyToMarket = applyToMarket;
     }
 
-    public Filter withApplyToMarket(Boolean applyToMarket) {
-        this.applyToMarket = applyToMarket;
-        return this;
+    @JsonProperty("avgPriceMins")
+    public Integer getAvgPriceMins() {
+        return avgPriceMins;
     }
 
-    public Long getLimit() {
+    @JsonProperty("avgPriceMins")
+    public void setAvgPriceMins(Integer avgPriceMins) {
+        this.avgPriceMins = avgPriceMins;
+    }
+
+    @JsonProperty("limit")
+    public Integer getLimit() {
         return limit;
     }
 
-    public void setLimit(Long limit) {
+    @JsonProperty("limit")
+    public void setLimit(Integer limit) {
         this.limit = limit;
     }
 
-    public Filter withLimit(Long limit) {
-        this.limit = limit;
-        return this;
+    @JsonProperty("minTrailingAboveDelta")
+    public Integer getMinTrailingAboveDelta() {
+        return minTrailingAboveDelta;
     }
 
-    public Long getMaxNumOrders() {
+    @JsonProperty("minTrailingAboveDelta")
+    public void setMinTrailingAboveDelta(Integer minTrailingAboveDelta) {
+        this.minTrailingAboveDelta = minTrailingAboveDelta;
+    }
+
+    @JsonProperty("maxTrailingAboveDelta")
+    public Integer getMaxTrailingAboveDelta() {
+        return maxTrailingAboveDelta;
+    }
+
+    @JsonProperty("maxTrailingAboveDelta")
+    public void setMaxTrailingAboveDelta(Integer maxTrailingAboveDelta) {
+        this.maxTrailingAboveDelta = maxTrailingAboveDelta;
+    }
+
+    @JsonProperty("minTrailingBelowDelta")
+    public Integer getMinTrailingBelowDelta() {
+        return minTrailingBelowDelta;
+    }
+
+    @JsonProperty("minTrailingBelowDelta")
+    public void setMinTrailingBelowDelta(Integer minTrailingBelowDelta) {
+        this.minTrailingBelowDelta = minTrailingBelowDelta;
+    }
+
+    @JsonProperty("maxTrailingBelowDelta")
+    public Integer getMaxTrailingBelowDelta() {
+        return maxTrailingBelowDelta;
+    }
+
+    @JsonProperty("maxTrailingBelowDelta")
+    public void setMaxTrailingBelowDelta(Integer maxTrailingBelowDelta) {
+        this.maxTrailingBelowDelta = maxTrailingBelowDelta;
+    }
+
+    @JsonProperty("bidMultiplierUp")
+    public String getBidMultiplierUp() {
+        return bidMultiplierUp;
+    }
+
+    @JsonProperty("bidMultiplierUp")
+    public void setBidMultiplierUp(String bidMultiplierUp) {
+        this.bidMultiplierUp = bidMultiplierUp;
+    }
+
+    @JsonProperty("bidMultiplierDown")
+    public String getBidMultiplierDown() {
+        return bidMultiplierDown;
+    }
+
+    @JsonProperty("bidMultiplierDown")
+    public void setBidMultiplierDown(String bidMultiplierDown) {
+        this.bidMultiplierDown = bidMultiplierDown;
+    }
+
+    @JsonProperty("askMultiplierUp")
+    public String getAskMultiplierUp() {
+        return askMultiplierUp;
+    }
+
+    @JsonProperty("askMultiplierUp")
+    public void setAskMultiplierUp(String askMultiplierUp) {
+        this.askMultiplierUp = askMultiplierUp;
+    }
+
+    @JsonProperty("askMultiplierDown")
+    public String getAskMultiplierDown() {
+        return askMultiplierDown;
+    }
+
+    @JsonProperty("askMultiplierDown")
+    public void setAskMultiplierDown(String askMultiplierDown) {
+        this.askMultiplierDown = askMultiplierDown;
+    }
+
+    @JsonProperty("maxNumOrders")
+    public Integer getMaxNumOrders() {
         return maxNumOrders;
     }
 
-    public void setMaxNumOrders(Long maxNumOrders) {
+    @JsonProperty("maxNumOrders")
+    public void setMaxNumOrders(Integer maxNumOrders) {
         this.maxNumOrders = maxNumOrders;
     }
 
-    public Filter withMaxNumOrders(Long maxNumOrders) {
-        this.maxNumOrders = maxNumOrders;
-        return this;
-    }
-
-    public Long getMaxNumAlgoOrders() {
+    @JsonProperty("maxNumAlgoOrders")
+    public Integer getMaxNumAlgoOrders() {
         return maxNumAlgoOrders;
     }
 
-    public void setMaxNumAlgoOrders(Long maxNumAlgoOrders) {
+    @JsonProperty("maxNumAlgoOrders")
+    public void setMaxNumAlgoOrders(Integer maxNumAlgoOrders) {
         this.maxNumAlgoOrders = maxNumAlgoOrders;
-    }
-
-    public Filter withMaxNumAlgoOrders(Long maxNumAlgoOrders) {
-        this.maxNumAlgoOrders = maxNumAlgoOrders;
-        return this;
     }
 
 }
--- src/main/java/pojo/Symbol.java
+++ src/main/java/pojo/Symbol.java
@@ -1,311 +1,263 @@
 package pojo;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.List;
 
 /*******JSON Mapping to Pojo Class for api/v3/exchangeInfo********/
 public class Symbol {
 
+    @JsonProperty("symbol")
     private String symbol;
+    @JsonProperty("status")
     private String status;
+    @JsonProperty("baseAsset")
     private String baseAsset;
-    private Long baseAssetPrecision;
+    @JsonProperty("baseAssetPrecision")
+    private Integer baseAssetPrecision;
+    @JsonProperty("quoteAsset")
     private String quoteAsset;
-    private Long quotePrecision;
-    private Long quoteAssetPrecision;
-    private Long baseCommissionPrecision;
-    private Long quoteCommissionPrecision;
+    @JsonProperty("quotePrecision")
+    private Integer quotePrecision;
+    @JsonProperty("quoteAssetPrecision")
+    private Integer quoteAssetPrecision;
+    @JsonProperty("baseCommissionPrecision")
+    private Integer baseCommissionPrecision;
+    @JsonProperty("quoteCommissionPrecision")
+    private Integer quoteCommissionPrecision;
+    @JsonProperty("orderTypes")
     private List<String> orderTypes = null;
+    @JsonProperty("icebergAllowed")
     private Boolean icebergAllowed;
+    @JsonProperty("ocoAllowed")
     private Boolean ocoAllowed;
+    @JsonProperty("quoteOrderQtyMarketAllowed")
     private Boolean quoteOrderQtyMarketAllowed;
+    @JsonProperty("allowTrailingStop")
     private Boolean allowTrailingStop;
+    @JsonProperty("cancelReplaceAllowed")
+    private Boolean cancelReplaceAllowed;
+    @JsonProperty("isSpotTradingAllowed")
     private Boolean isSpotTradingAllowed;
+    @JsonProperty("isMarginTradingAllowed")
     private Boolean isMarginTradingAllowed;
+    @JsonProperty("filters")
     private List<Filter> filters = null;
+    @JsonProperty("permissions")
     private List<String> permissions = null;
+    @JsonProperty("defaultSelfTradePreventionMode")
+    private String defaultSelfTradePreventionMode;
+    @JsonProperty("allowedSelfTradePreventionModes")
+    private List<String> allowedSelfTradePreventionModes = null;
 
-    /**
-     * No args constructor for use in serialization
-     *
-     */
-    public Symbol() {
-    }
-
-    /**
-     *
-     * @param symbol
-     * @param quoteOrderQtyMarketAllowed
-     * @param allowTrailingStop
-     * @param baseAsset
-     * @param filters
-     * @param baseAssetPrecision
-     * @param isSpotTradingAllowed
-     * @param quoteAssetPrecision
-     * @param quoteCommissionPrecision
-     * @param ocoAllowed
-     * @param quotePrecision
-     * @param orderTypes
-     * @param permissions
-     * @param icebergAllowed
-     * @param isMarginTradingAllowed
-     * @param quoteAsset
-     * @param baseCommissionPrecision
-     * @param status
-     */
-    public Symbol(String symbol, String status, String baseAsset, Long baseAssetPrecision, String quoteAsset, Long quotePrecision, Long quoteAssetPrecision, Long baseCommissionPrecision, Long quoteCommissionPrecision, List<String> orderTypes, Boolean icebergAllowed, Boolean ocoAllowed, Boolean quoteOrderQtyMarketAllowed, Boolean allowTrailingStop, Boolean isSpotTradingAllowed, Boolean isMarginTradingAllowed, List<Filter> filters, List<String> permissions) {
-        super();
-        this.symbol = symbol;
-        this.status = status;
-        this.baseAsset = baseAsset;
-        this.baseAssetPrecision = baseAssetPrecision;
-        this.quoteAsset = quoteAsset;
-        this.quotePrecision = quotePrecision;
-        this.quoteAssetPrecision = quoteAssetPrecision;
-        this.baseCommissionPrecision = baseCommissionPrecision;
-        this.quoteCommissionPrecision = quoteCommissionPrecision;
-        this.orderTypes = orderTypes;
-        this.icebergAllowed = icebergAllowed;
-        this.ocoAllowed = ocoAllowed;
-        this.quoteOrderQtyMarketAllowed = quoteOrderQtyMarketAllowed;
-        this.allowTrailingStop = allowTrailingStop;
-        this.isSpotTradingAllowed = isSpotTradingAllowed;
-        this.isMarginTradingAllowed = isMarginTradingAllowed;
-        this.filters = filters;
-        this.permissions = permissions;
-    }
-
+    @JsonProperty("symbol")
     public String getSymbol() {
         return symbol;
     }
 
+    @JsonProperty("symbol")
     public void setSymbol(String symbol) {
         this.symbol = symbol;
     }
 
-    public Symbol withSymbol(String symbol) {
-        this.symbol = symbol;
-        return this;
-    }
-
+    @JsonProperty("status")
     public String getStatus() {
         return status;
     }
 
+    @JsonProperty("status")
     public void setStatus(String status) {
         this.status = status;
     }
 
-    public Symbol withStatus(String status) {
-        this.status = status;
-        return this;
-    }
-
+    @JsonProperty("baseAsset")
     public String getBaseAsset() {
         return baseAsset;
     }
 
+    @JsonProperty("baseAsset")
     public void setBaseAsset(String baseAsset) {
         this.baseAsset = baseAsset;
     }
 
-    public Symbol withBaseAsset(String baseAsset) {
-        this.baseAsset = baseAsset;
-        return this;
-    }
-
-    public Long getBaseAssetPrecision() {
+    @JsonProperty("baseAssetPrecision")
+    public Integer getBaseAssetPrecision() {
         return baseAssetPrecision;
     }
 
-    public void setBaseAssetPrecision(Long baseAssetPrecision) {
+    @JsonProperty("baseAssetPrecision")
+    public void setBaseAssetPrecision(Integer baseAssetPrecision) {
         this.baseAssetPrecision = baseAssetPrecision;
     }
 
-    public Symbol withBaseAssetPrecision(Long baseAssetPrecision) {
-        this.baseAssetPrecision = baseAssetPrecision;
-        return this;
-    }
-
+    @JsonProperty("quoteAsset")
     public String getQuoteAsset() {
         return quoteAsset;
     }
 
+    @JsonProperty("quoteAsset")
     public void setQuoteAsset(String quoteAsset) {
         this.quoteAsset = quoteAsset;
     }
 
-    public Symbol withQuoteAsset(String quoteAsset) {
-        this.quoteAsset = quoteAsset;
-        return this;
-    }
-
-    public Long getQuotePrecision() {
+    @JsonProperty("quotePrecision")
+    public Integer getQuotePrecision() {
         return quotePrecision;
     }
 
-    public void setQuotePrecision(Long quotePrecision) {
+    @JsonProperty("quotePrecision")
+    public void setQuotePrecision(Integer quotePrecision) {
         this.quotePrecision = quotePrecision;
     }
 
-    public Symbol withQuotePrecision(Long quotePrecision) {
-        this.quotePrecision = quotePrecision;
-        return this;
-    }
-
-    public Long getQuoteAssetPrecision() {
+    @JsonProperty("quoteAssetPrecision")
+    public Integer getQuoteAssetPrecision() {
         return quoteAssetPrecision;
     }
 
-    public void setQuoteAssetPrecision(Long quoteAssetPrecision) {
+    @JsonProperty("quoteAssetPrecision")
+    public void setQuoteAssetPrecision(Integer quoteAssetPrecision) {
         this.quoteAssetPrecision = quoteAssetPrecision;
     }
 
-    public Symbol withQuoteAssetPrecision(Long quoteAssetPrecision) {
-        this.quoteAssetPrecision = quoteAssetPrecision;
-        return this;
-    }
-
-    public Long getBaseCommissionPrecision() {
+    @JsonProperty("baseCommissionPrecision")
+    public Integer getBaseCommissionPrecision() {
         return baseCommissionPrecision;
     }
 
-    public void setBaseCommissionPrecision(Long baseCommissionPrecision) {
+    @JsonProperty("baseCommissionPrecision")
+    public void setBaseCommissionPrecision(Integer baseCommissionPrecision) {
         this.baseCommissionPrecision = baseCommissionPrecision;
     }
 
-    public Symbol withBaseCommissionPrecision(Long baseCommissionPrecision) {
-        this.baseCommissionPrecision = baseCommissionPrecision;
-        return this;
-    }
-
-    public Long getQuoteCommissionPrecision() {
+    @JsonProperty("quoteCommissionPrecision")
+    public Integer getQuoteCommissionPrecision() {
         return quoteCommissionPrecision;
     }
 
-    public void setQuoteCommissionPrecision(Long quoteCommissionPrecision) {
+    @JsonProperty("quoteCommissionPrecision")
+    public void setQuoteCommissionPrecision(Integer quoteCommissionPrecision) {
         this.quoteCommissionPrecision = quoteCommissionPrecision;
     }
 
-    public Symbol withQuoteCommissionPrecision(Long quoteCommissionPrecision) {
-        this.quoteCommissionPrecision = quoteCommissionPrecision;
-        return this;
-    }
-
+    @JsonProperty("orderTypes")
     public List<String> getOrderTypes() {
         return orderTypes;
     }
 
+    @JsonProperty("orderTypes")
     public void setOrderTypes(List<String> orderTypes) {
         this.orderTypes = orderTypes;
     }
 
-    public Symbol withOrderTypes(List<String> orderTypes) {
-        this.orderTypes = orderTypes;
-        return this;
-    }
-
+    @JsonProperty("icebergAllowed")
     public Boolean getIcebergAllowed() {
         return icebergAllowed;
     }
 
+    @JsonProperty("icebergAllowed")
     public void setIcebergAllowed(Boolean icebergAllowed) {
         this.icebergAllowed = icebergAllowed;
     }
 
-    public Symbol withIcebergAllowed(Boolean icebergAllowed) {
-        this.icebergAllowed = icebergAllowed;
-        return this;
-    }
-
+    @JsonProperty("ocoAllowed")
     public Boolean getOcoAllowed() {
         return ocoAllowed;
     }
 
+    @JsonProperty("ocoAllowed")
     public void setOcoAllowed(Boolean ocoAllowed) {
         this.ocoAllowed = ocoAllowed;
     }
 
-    public Symbol withOcoAllowed(Boolean ocoAllowed) {
-        this.ocoAllowed = ocoAllowed;
-        return this;
-    }
-
+    @JsonProperty("quoteOrderQtyMarketAllowed")
     public Boolean getQuoteOrderQtyMarketAllowed() {
         return quoteOrderQtyMarketAllowed;
     }
 
+    @JsonProperty("quoteOrderQtyMarketAllowed")
     public void setQuoteOrderQtyMarketAllowed(Boolean quoteOrderQtyMarketAllowed) {
         this.quoteOrderQtyMarketAllowed = quoteOrderQtyMarketAllowed;
     }
 
-    public Symbol withQuoteOrderQtyMarketAllowed(Boolean quoteOrderQtyMarketAllowed) {
-        this.quoteOrderQtyMarketAllowed = quoteOrderQtyMarketAllowed;
-        return this;
-    }
-
+    @JsonProperty("allowTrailingStop")
     public Boolean getAllowTrailingStop() {
         return allowTrailingStop;
     }
 
+    @JsonProperty("allowTrailingStop")
     public void setAllowTrailingStop(Boolean allowTrailingStop) {
         this.allowTrailingStop = allowTrailingStop;
     }
 
-    public Symbol withAllowTrailingStop(Boolean allowTrailingStop) {
-        this.allowTrailingStop = allowTrailingStop;
-        return this;
+    @JsonProperty("cancelReplaceAllowed")
+    public Boolean getCancelReplaceAllowed() {
+        return cancelReplaceAllowed;
     }
 
+    @JsonProperty("cancelReplaceAllowed")
+    public void setCancelReplaceAllowed(Boolean cancelReplaceAllowed) {
+        this.cancelReplaceAllowed = cancelReplaceAllowed;
+    }
+
+    @JsonProperty("isSpotTradingAllowed")
     public Boolean getIsSpotTradingAllowed() {
         return isSpotTradingAllowed;
     }
 
+    @JsonProperty("isSpotTradingAllowed")
     public void setIsSpotTradingAllowed(Boolean isSpotTradingAllowed) {
         this.isSpotTradingAllowed = isSpotTradingAllowed;
     }
 
-    public Symbol withIsSpotTradingAllowed(Boolean isSpotTradingAllowed) {
-        this.isSpotTradingAllowed = isSpotTradingAllowed;
-        return this;
-    }
-
+    @JsonProperty("isMarginTradingAllowed")
     public Boolean getIsMarginTradingAllowed() {
         return isMarginTradingAllowed;
     }
 
+    @JsonProperty("isMarginTradingAllowed")
     public void setIsMarginTradingAllowed(Boolean isMarginTradingAllowed) {
         this.isMarginTradingAllowed = isMarginTradingAllowed;
     }
 
-    public Symbol withIsMarginTradingAllowed(Boolean isMarginTradingAllowed) {
-        this.isMarginTradingAllowed = isMarginTradingAllowed;
-        return this;
-    }
-
+    @JsonProperty("filters")
     public List<Filter> getFilters() {
         return filters;
     }
 
+    @JsonProperty("filters")
     public void setFilters(List<Filter> filters) {
         this.filters = filters;
     }
 
-    public Symbol withFilters(List<Filter> filters) {
-        this.filters = filters;
-        return this;
-    }
-
+    @JsonProperty("permissions")
     public List<String> getPermissions() {
         return permissions;
     }
 
+    @JsonProperty("permissions")
     public void setPermissions(List<String> permissions) {
         this.permissions = permissions;
     }
 
-    public Symbol withPermissions(List<String> permissions) {
-        this.permissions = permissions;
-        return this;
+    @JsonProperty("defaultSelfTradePreventionMode")
+    public String getDefaultSelfTradePreventionMode() {
+        return defaultSelfTradePreventionMode;
+    }
+
+    @JsonProperty("defaultSelfTradePreventionMode")
+    public void setDefaultSelfTradePreventionMode(String defaultSelfTradePreventionMode) {
+        this.defaultSelfTradePreventionMode = defaultSelfTradePreventionMode;
+    }
+
+    @JsonProperty("allowedSelfTradePreventionModes")
+    public List<String> getAllowedSelfTradePreventionModes() {
+        return allowedSelfTradePreventionModes;
+    }
+
+    @JsonProperty("allowedSelfTradePreventionModes")
+    public void setAllowedSelfTradePreventionModes(List<String> allowedSelfTradePreventionModes) {
+        this.allowedSelfTradePreventionModes = allowedSelfTradePreventionModes;
     }
 
 }
